2788ec5b5b1dd7579ae7b36f65fc93d18f23d1bf
JoeAndrawis <https://github.com/JoeAndrawis>
2026-04-12 21:51:44 +0200

    feat(activity-service): fix update 500 and summary 404 (40-20092)

--- activity-service/src/main/java/com/randmteam2/tripplanning/activity/model/Activity.java
+++ activity-service/src/main/java/com/randmteam2/tripplanning/activity/model/Activity.java
@@ -21,7 +21,7 @@ public class Activity {
     private String name;
 
     @Enumerated(EnumType.STRING)
-    @Column(columnDefinition = "activity_category")
+    @Column(nullable = false)
     private Category category;
 
     @Column(nullable = false)
