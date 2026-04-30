b8a57f9db9da18b046a90cd84f702259bb926e87
Salma Yasser Abdelmageed Meckawy <Salma20020418@users.noreply.github.com>
2026-04-30 23:01:16 +0300

    feat/booking/CC-BK1/68-38306

--- booking-service/src/main/java/com/randmteam2/tripplanning/booking/security/SecurityConfig.java
+++ booking-service/src/main/java/com/randmteam2/tripplanning/booking/security/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.randmteam2.tripplanning.booking.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -16,10 +15,9 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @EnableWebSecurity
 public class SecurityConfig {
 
-    @Autowired private JwtAuthenticationFilter jwtFilter;
-
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http,
+                                           JwtAuthenticationFilter jwtFilter) throws Exception {
         return http
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
