25147acd6a2421b81a508573ad457ca383b702e4
mostafah404 <mostafa.ahmed1997@icloud.com>
2026-05-18 15:17:26 +0300

    fix(user/S1-EVENTS): wire Jackson message converter and fix cache eviction prefix

--- user-service/src/main/java/com/randmteam2/tripplanning/user/config/RabbitMqTopologyConfig.java
+++ user-service/src/main/java/com/randmteam2/tripplanning/user/config/RabbitMqTopologyConfig.java
@@ -1,6 +1,9 @@
 package com.randmteam2.tripplanning.user.config;
 
 import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,6 +11,20 @@ import org.springframework.context.annotation.Configuration;
 public class RabbitMqTopologyConfig {
 
     // Producer: user-service publishes to this exchange
+    @Bean
+    public Jackson2JsonMessageConverter jackson2JsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(
+            ConnectionFactory connectionFactory) {
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setMessageConverter(jackson2JsonMessageConverter());
+        return factory;
+    }
+
     @Bean
     public TopicExchange userEventsExchange() {
         return new TopicExchange("user.events", true, false);
